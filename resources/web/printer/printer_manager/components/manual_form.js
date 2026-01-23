95edf26eae28a5c0df785e10be388710d92ffd64
anjis <anjis.zhou@elegoo.com>
2026-01-23 19:29:12 +0800

    limited the maximum length of the PIN code input.

--- resources/web/printer/printer_manager/components/manual_form.js
+++ resources/web/printer/printer_manager/components/manual_form.js
@@ -146,7 +146,7 @@ const ManualFormTemplate = /*html*/
                                 type="password"
                                 v-model="formData.pinCode"
                                 :placeholder="$t('manualForm.pinCodePlaceholderRequired')"
-                                maxlength="20"
+                                maxlength="6"
                                 show-password
                             />
                         </el-form-item>
@@ -339,6 +339,10 @@ const ManualFormComponent = {
                             }
                             if (!value || !value.trim()) {
                                 callback(new Error(this.$t('manualForm.pleaseEnterPinCode')));
+                                return;
+                            }
+                            if (value.trim().length < 6) {
+                                callback(new Error(this.$t('manualForm.pinCodeMustBe6Characters')));
                                 return;
                             }
                             callback();
