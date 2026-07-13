7a5837fe3656e394d6467abcec0980e73aa56661
wujie <jie.wu@elegoo.com>
2026-07-13 19:26:24 +0800

    fix(gui): add null check for m_plater in check_outside to prevent crash

--- src/slic3r/GUI/PartPlate.cpp
+++ src/slic3r/GUI/PartPlate.cpp
@@ -2630,7 +2630,8 @@ bool PartPlate::check_outside(int obj_id, int instance_id, BoundingBoxf3* boundi
 		// Orca: For sinking object, we use a more expensive algorithm so part below build plate won't be considered
 		if (plate_box.intersects(instance_box)) {
 			// TODO: FIXME: this does not take exclusion area into account
-            const BuildVolume build_volume(get_shape(), m_plater->build_volume().printable_height(), m_extruder_areas, m_extruder_heights);
+            double print_height = m_plater ? m_plater->build_volume().printable_height() : (double)m_height;
+            const BuildVolume build_volume(get_shape(), print_height, m_extruder_areas, m_extruder_heights);
 			const auto state = instance->calc_print_volume_state(build_volume);
 			outside = state == ModelInstancePVS_Partly_Outside;
 		}
